50843c1a2e4d9bfeaed6deb99fd543750f4b3f74
mebrah <mebrahtubereket@gmail.com>
2023-10-02 22:25:49 +0100

    Adds album repository tests

--- lib/album_repository.py
+++ lib/album_repository.py
@@ -12,3 +12,16 @@ class AlbumRepository:
             item = Album(row["title"], row["release_year"],row["artist_id"])
             albums.append(item)
         return albums
+    def find(self, artist_id):
+        rows = self._connection.execute('SELECT title FROM albums WHERE id= %s', [artist_id])
+        row = rows[0]
+        return Album(row["title"], row["release_year"])
+    def create(self, artist):
+        self._connection.execute('INSERT INTO artist (name, title) VALUES (%s, %s)', [artist.name, artist.title])
+        return None
+    def delete(self, artist_id):
+        self._connection.execute(
+            'DELETE FROM albums WHERE is = %s', [artist_id]
+        )
+        return None
+
