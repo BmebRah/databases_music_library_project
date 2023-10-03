14214bfcc56aee19caac8cac176ce225ffe30d1a
mebrah <mebrahtubereket@gmail.com>
2023-10-03 12:27:29 +0100

    Completes test drive repository class

--- lib/album.py
+++ lib/album.py
@@ -1,5 +1,6 @@
 class Album:
-    def __init__(self, title, release_year, artist_id):
+    def __init__(self, id, title, release_year, artist_id):
+        self.id = id
         self.title = title
         self.release_year = release_year
         self.artist_id = artist_id
@@ -11,4 +12,4 @@ class Album:
 
     # This method makes it look nicer when we print an Artist
     def __repr__(self):
-        return f"Album({self.title}, {self.release_year}, {self.artist_id})"
+        return f"Album({self.id}, {self.title}, {self.release_year}, {self.artist_id})"
--- lib/album_repository.py
+++ lib/album_repository.py
@@ -9,19 +9,19 @@ class AlbumRepository:
         rows = self._connection.execute("SELECT * FROM albums")
         albums = []
         for row in rows:
-            item = Album(row["title"], row["release_year"],row["artist_id"])
+            item = Album(row["id"], row["title"], row["release_year"],row["artist_id"])
             albums.append(item)
         return albums
     def find(self, artist_id):
         rows = self._connection.execute('SELECT title FROM albums WHERE id= %s', [artist_id])
         row = rows[0]
-        return Album(row["title"], row["release_year"])
+        return Album(row["id"],row["title"], row["release_year"])
     def create(self, artist):
         self._connection.execute('INSERT INTO artist (name, title) VALUES (%s, %s)', [artist.name, artist.title])
         return None
     def delete(self, artist_id):
         self._connection.execute(
-            'DELETE FROM albums WHERE is = %s', [artist_id]
+            'DELETE FROM albums WHERE id = %s', [artist_id]
         )
         return None
 
